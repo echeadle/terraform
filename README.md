c2f65d6a978dab1f5b9d3fa034645c186f78e52c
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-08-04 11:39:26 -0600

    Replaced instance with asg and doc instance by adding it to readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 Terraform Up and Running
+
+ Instance Example removed and replace by launch configuraiton
+resource "aws_instance" "example" {
+#  ami = "ami-a9d09ed1"
+  ami = "ami-1cc69e64"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  tags {
+    Name = "terraform-example"
+  }
+}
--- main.tf
+++ main.tf
@@ -8,19 +8,17 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "example" {
-#  ami = "ami-a9d09ed1"
-  ami = "ami-1cc69e64"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-1cc69e64"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-
+  security_groups = ["${aws_security_group.instance.id}"]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -32,4 +30,23 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  min_size = 2
+  max_size = 10
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+  
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
