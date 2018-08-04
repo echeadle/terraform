416dbc9cf87d30215536e80ab52ae4b6399bf9b7
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-08-04 10:16:54 -0600

    Added server port variable

--- main.tf
+++ main.tf
@@ -3,16 +3,21 @@ provider "aws" {
   profile = "terraform"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
 resource "aws_instance" "example" {
 #  ami = "ami-a9d09ed1"
   ami = "ami-1cc69e64"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
   tags {
     Name = "terraform-example"
@@ -22,8 +27,8 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
