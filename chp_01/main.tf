1e352d45bc1ca87753ed659357b63b6dc4c7b585
Edward Cheadle <edward.cheadle8@gmail.com>
2019-10-02 17:54:01 -0600

    Started over

--- chp_01/main.tf
+++ chp_01/main.tf
@@ -1,52 +1,35 @@
 provider "aws" {
   region = "us-west-2"
-  profile = "terraform"
 }
 
-variable "server_port" {
+variable "server_port" {  
   description = "The port the server will use for HTTP requests"
-  default = 8080
+    type        = number
+
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-1cc69e64"
+resource "aws_instance" "example" {
+
+  ami           = "ami-06f2f779464715dc5"
   instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
-  lifecycle {
-    create_before_destroy = true
+            #!/bin/bash
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+  tags = {
+    Name = "terraform-example"
   }
 }
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
+
   ingress {
-    from_port = "${var.server_port}"
-    to_port = "${var.server_port}"
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.id}"
-  min_size = 2
-  max_size = 10
-  tag {
-    key = "Name"
-    value = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-  
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
 }
